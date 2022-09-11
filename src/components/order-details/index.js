a1008953b4ca5c1d5a59bf15b14dd2ec05aff970
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-09-11 20:55:59 +0300

    добавлен alt для img "Done"

--- src/components/order-details/index.js
+++ src/components/order-details/index.js
@@ -9,7 +9,7 @@ function IngredientOrderModal({ orderId, status, onClose, name }) {
       <div className={`${Styles['order-id']} mt-16 text text_type_main-large`}>{orderId}</div>
       <div className={`${Styles['sub-title']} mt-8 text text_type_main-default`}>идентификатор заказа</div>
       <div className={`${Styles['sub-title']} mt-15`}>
-        <img width={120} height={120} src={Done} />
+        <img alt={'Готово'} width={120} height={120} src={Done} />
       </div>
       <div className={`${Styles['sub-title']} mt-15 text text_type_main-default`}>{status}</div>
       <div className={`${Styles['sub-title']} mt-2 text text_type_main-default text_color_inactive`}>Дождитесь готовности на орбитальной станции</div>
