eb8e00a1677d1158302c329b1aa6f0af2beabb9b
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-09-11 21:57:01 +0300

    добавлена типизация для фона закрытия модального окна

--- src/components/modal-overlay/index.js
+++ src/components/modal-overlay/index.js
@@ -1,9 +1,14 @@
+import { func } from 'prop-types';
 import Styles from './style.module.scss';
 
 function BlackDropModal({ onClose }) {
   return (<div
     onClick={onClose}
     className={`${Styles.container}`}></div>)
+}
+
+BlackDropModal.propTypes = {
+  onClose: func
 }
 
 export default BlackDropModal;
