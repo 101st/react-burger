d031ffe083d9de68a5cf5b258b80a651251fd59e
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-09-12 22:16:54 +0300

    убран эл. mock данных, т.к. булка только одного вида

--- src/utils/data.js
+++ src/utils/data.js
@@ -82,19 +82,5 @@ export const constructorData = [
     "image_mobile": "https://code.s3.yandex.net/react/code/mineral_rings-mobile.png",
     "image_large": "https://code.s3.yandex.net/react/code/mineral_rings-large.png",
     "__v": 0
-  },
-  {
-    "_id": "60666c42cc7b410027a1a9b1",
-    "name": "Краторная булка N-200i",
-    "type": "bun",
-    "proteins": 80,
-    "fat": 24,
-    "carbohydrates": 53,
-    "calories": 420,
-    "price": 1255,
-    "image": "https://code.s3.yandex.net/react/code/bun-02.png",
-    "image_mobile": "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-    "image_large": "https://code.s3.yandex.net/react/code/bun-02-large.png",
-    "__v": 0
-  },
+  }
 ]
