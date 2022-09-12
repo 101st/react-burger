f28b568003d46ead6475e62a9a4c9ec5e9b4d2bd
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-09-12 21:54:26 +0300

    доработка механизма работы модального окна

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -6,30 +6,32 @@ import ModalBackDrop from '../ModalOverlay/modalOverlay';
 import Styles from './style.module.scss';
 import { useEffect } from 'react';
 
-function Modal({ onClose, header, setModalVisible, children }) {
+function Modal({ onClose, title, isOpen, children }) {
 
   useEffect(() => {
     function closeByEscape(e) {
-      if (e.key === 'Escape') setModalVisible(false);
+      if (e.key === 'Escape') onClose(false);
     }
 
     document.addEventListener('keydown', closeByEscape);
-    
+
     return () => {
       document.removeEventListener('keydown', closeByEscape);
     }
-  }, [onClose, setModalVisible])
+  }, [onClose])
 
-  return ReactDOM.createPortal(
-    <div className={`${Styles.container}`}>
-      <div className={`${Styles.modal} pt-10 pr-10 pl-10 pb-15`}>
-        <ModalHeader onClose={onClose}>{header}</ModalHeader>
-        {children}
-      </div>
-      <ModalBackDrop onClose={onClose} />
-    </div>,
-    document.getElementById("react-modals")
-  );
+  if (isOpen)
+    return ReactDOM.createPortal(
+      <div className={`${Styles.container}`}>
+        <div className={`${Styles.modal} pt-10 pr-10 pl-10 pb-15`}>
+          <ModalHeader onClose={onClose}>{title}</ModalHeader>
+          {children}
+        </div>
+        <ModalBackDrop onClose={onClose} />
+      </div>,
+      document.getElementById("react-modals")
+    );
+  return;
 }
 
 Modal.propTypes = {
