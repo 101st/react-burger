9c4c2902797ff9c11f436d3ae31410e94a41040b
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-09-12 21:57:56 +0300

    модальное окно убрано из компонента ингредиента конструктора

--- src/components/BurgerConstructor/item.js
+++ src/components/BurgerConstructor/item.js
@@ -1,60 +1,30 @@
-import { useState } from 'react';
 import { object } from 'prop-types';
 import { ingredientType } from '../../utils/types';
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
-import IngredientDetailModal from '../IngredientDetails/ingredientDetails';
 
 import Styles from './style.module.scss';
 
 function BurgerConstructorItem({
-  image_mobile,
-  image_large,
-  name,
-  type,
-  price,
-  calories,
-  fat,
-  isLocked,
-  carbohydrates,
-  proteins,
+  currentItem
 }) {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const getType = type => {
-    if (type === 'top') return ' (верх)';
-    if (type === 'bottom') return ' (низ)';
-    return '';
-  }
-
-  const handleOpenModal = () => {
-    setModalVisible(true);
-  }
-
-  const handleCloseModal = (e) => {
-    e.stopPropagation();
-    setModalVisible(false);
-  }
+  const {
+    image_mobile,
+    name,
+    type,
+    price,
+    isLocked,
+  } = currentItem;
 
   return (
-    <div className={`${Styles['in-stack']} mb-4 ml-8 mr-4`} onClick={handleOpenModal}>
+    <div className={`${Styles['in-stack']} mb-4 ml-8 mr-4`}>
       <ConstructorElement
         className={`mb-4`}
-        text={`${name}${getType(type)}`}
+        text={`${name}`}
         isLocked={isLocked}
         type={type}
         price={price}
         thumbnail={image_mobile}
       />
-      {modalVisible && <IngredientDetailModal
-        onClose={handleCloseModal}
-        setModalVisible={setModalVisible}
-        name={name}
-        image_large={image_large}
-        calories={calories}
-        fat={fat}
-        carbohydrates={carbohydrates}
-        proteins={proteins}
-      />}
     </div>
   )
 }
