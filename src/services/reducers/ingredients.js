f4c96014646ea2eb646b62eb346b97e2ea0dc613
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-02 00:44:09 +0300

    Добавлены reducers для ингредиентов

---
+++ src/services/reducers/ingredients.js
@@ -0,0 +1,56 @@
+import {
+  GET_INGRIDIENTS_REQUEST,
+  GET_INGRIDIENTS_SUCCESS,
+  GET_INGRIDIENTS_FAILED,
+  SET_CURRENT_INGREDIENT_TYPE,
+  SET_INGRIDIENTS_STORE,
+} from "../actions/ingredients";
+
+const initialState = {
+  ingredients: [],
+  ingredientsStore: [],
+  getIngredientsRequest: false,
+  getIngredientsFailed: false,
+  currentIngredientType: 'main'
+};
+
+export const ingredientsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_INGRIDIENTS_REQUEST: {
+      return {
+        ...state,
+        getIngredientsRequest: true,
+        getIngredientsFailed: false,
+      };
+    }
+    case GET_INGRIDIENTS_SUCCESS: {
+      return {
+        ...state,
+        getIngredientsRequest: false,
+        ingredients: action.ingredients,
+      };
+    }
+    case GET_INGRIDIENTS_FAILED: {
+      return {
+        ...state,
+        getIngredientsRequest: false,
+        getIngredientsFailed: true,
+      };
+    }
+    case SET_CURRENT_INGREDIENT_TYPE: {
+      return {
+        ...state,
+        currentIngredientType: action.currentIngredientType,
+      };
+    }
+    case SET_INGRIDIENTS_STORE: {
+      return {
+        ...state,
+        ingredientsStore: state.ingredients.filter(item => item.type === action.currentIngredientType)
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
