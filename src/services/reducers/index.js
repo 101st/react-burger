d1bbf6594fe1dfd68f612e868488085dd0667f98
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-02 00:44:29 +0300

    Добавлен комбайнер reducer-ов

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import { ingredientsReducer } from './ingredients';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  ingredients: ingredientsReducer
+});
