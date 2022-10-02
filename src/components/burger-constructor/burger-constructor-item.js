63df8e2376020465b3ae77dd39c09e330fdd2116
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-02 07:04:53 +0300

    реф

--- src/components/burger-constructor/burger-constructor-item.js
+++ src/components/burger-constructor/burger-constructor-item.js
@@ -8,31 +8,23 @@ function BurgerConstructorItem({
   currentItem,
   isDraggable,
 }) {
-  const {
-    image_mobile,
-    name,
-    type,
-    price,
-    isLocked,
-  } = currentItem;
-
   return (
     <div className={`${Styles['in-stack']} mb-4`}>
       {isDraggable && <div className='mr-2'><DragIcon type="primary" /></div>}
       <ConstructorElement
-        text={`${name}`}
-        isLocked={isLocked}
-        type={type}
-        price={price}
-        thumbnail={image_mobile}
+        text={`${currentItem.name}`}
+        isLocked={currentItem.isLocked}
+        type={currentItem.type}
+        price={currentItem.price}
+        thumbnail={currentItem.image_mobile}
       />
     </div>
   )
 }
 
-BurgerConstructorItem.propTypes = {
+/* BurgerConstructorItem.propTypes = {
   currentItem: shape(ingredientType).isRequired,
   isDraggable: bool,
-};
+}; */
 
 export default BurgerConstructorItem;
