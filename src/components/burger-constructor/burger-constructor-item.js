668bfee7e1ec7b5e2f918792a1cf27da70ce4c13
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-02 11:17:00 +0300

    реф и dnd для ингредиентов конструктора

--- src/components/burger-constructor/burger-constructor-item.js
+++ src/components/burger-constructor/burger-constructor-item.js
@@ -1,30 +1,56 @@
-import { bool, shape } from 'prop-types';
-import { ingredientType } from '../../utils/types';
+import { useRef } from "react";
+import { useDispatch } from 'react-redux';
+import { useDrag, useDrop } from 'react-dnd';
 import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { removeIngredient, dragIngredient } from "../../services/actions/constructor";
 
 import Styles from './style.module.scss';
 
-function BurgerConstructorItem({
-  currentItem,
-  isDraggable,
-}) {
+function BurgerConstructorItem({ ingredient, isDraggable, index }) {
+  const ref = useRef(null);
+  const dispatch = useDispatch();
+
+  const [, drop] = useDrop({
+    accept: "CONSTRUCTOR",
+    hover(ingredient) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = ingredient.index;
+      const hoverIndex = index;
+      dispatch(dragIngredient(dragIndex, hoverIndex, ingredient));
+      ingredient.index = hoverIndex;
+    },
+
+  });
+
+  const [{ opacity }, drag] = useDrag({
+    type: "CONSTRUCTOR",
+    item: { ...ingredient, index },
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+
+  drag(drop(ref));
+
   return (
-    <div className={`${Styles['in-stack']} mb-4`}>
+    <div
+      className={`${Styles['in-stack']} mb-4`}
+      ref={ref}
+      style={{ opacity }}
+    >
       {isDraggable && <div className='mr-2'><DragIcon type="primary" /></div>}
       <ConstructorElement
-        text={`${currentItem.name}`}
-        isLocked={currentItem.isLocked}
-        type={currentItem.type}
-        price={currentItem.price}
-        thumbnail={currentItem.image_mobile}
+        text={`${ingredient.name}`}
+        isLocked={ingredient.isLocked}
+        type={ingredient.type}
+        price={ingredient.price}
+        thumbnail={ingredient.image_mobile}
+        handleClose={() => dispatch(removeIngredient(ingredient))}
       />
     </div>
   )
 }
-
-/* BurgerConstructorItem.propTypes = {
-  currentItem: shape(ingredientType).isRequired,
-  isDraggable: bool,
-}; */
 
 export default BurgerConstructorItem;
