eb8d89056324195d609dd584698bdb6ed2b074f8
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-02 12:47:57 +0300

    Добавлены reducer и action для модального окна показа деталей ингредиента

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { ingredientsReducer } from './ingredients';
 import { constructorReducer } from './constructor';
+import { ingredientDetailsReducer } from './ingredient-details';
 
 export const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
   constructors: constructorReducer, // 's потосу что слово constructor зарезервировано
+  ingredientDetails: ingredientDetailsReducer,
 });
