62d76ced4497c1bfc44d58ee5d4d33fac18dd72c
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-22 18:31:28 +0300

    добавлена форма регистрации

---
+++ src/pages/register/register.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './register.module.scss';
+
+function Register() {
+  const [form, setValue] = useState({ name: '', email: '', password: '' });
+  const onChange = (e) => {
+    setValue({ ...form, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <div className={styles.container + ' mt-20 text_type_main-default'}>
+      <h1 className='mb-6'>Регистрация</h1>
+      <form className={styles.form} onSubmit={console.log}>
+        <Input
+          type='text'
+          placeholder='Имя'
+          value={form.name}
+          onChange={onChange}
+          name='name'
+        />
+        <Input
+          type='email'
+          placeholder='E-mail'
+          value={form.email}
+          onChange={onChange}
+          name='email'
+        />
+        <Input
+          type='password'
+          placeholder='Пароль'
+          value={form.password}
+          onChange={onChange}
+          icon='ShowIcon'
+          name='password'
+        />
+        <Button type='primary' htmlType='button' size='medium'>Зарегистрироваться</Button>
+      </form>
+      <div className={styles.links + ' text_color_inactive mt-20'}>
+        <p className='text text_type_main-small'>
+          Уже зарегистрированы? <Link to={'/login'}>Войти</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Register;
