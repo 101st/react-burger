c3f8cb207cbedd7f7df2e02b2ec3718a3805681e
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-24 02:37:52 +0300

    Доработка меню шапки сайта

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,23 +1,36 @@
-import { useState } from 'react';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
-import AppHeaderItem from './app-header-item';
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { useLocation, Link } from "react-router-dom";
 
 import Styles from './app-header.module.scss';
 
 function AppHeader() {
-  const [activeMenuItem, setActiveMenuItem] = useState('BurgerIcon');
+  const { pathname } = useLocation();
 
   return (
     <header className={`${Styles.container} text text_type_main-default`}>
       <div className='mt-4 mb-4'>
-        <AppHeaderItem id={'BurgerIcon'} name='Конструктор' activeMenuItem={activeMenuItem} setActiveMenuItem={setActiveMenuItem} />
-        <AppHeaderItem id={'ListIcon'} name='Лента заказов' activeMenuItem={activeMenuItem} setActiveMenuItem={setActiveMenuItem} />
+        <nav className='pl-5 pr-5 mr-2'>
+          <BurgerIcon type={pathname === '/' ? 'primary' : 'secondary'} />
+          <Link to='/' className={`ml-2 ${pathname !== '/' && 'text_color_inactive'}`}>Конструктор</Link>
+        </nav>
+        <nav className='pl-5 pr-5 mr-2'>
+          <ListIcon type={pathname === '/feed' ? 'primary' : 'secondary'} />
+          <Link to='/feed' className={`ml-2 ${pathname !== '/feed' && 'text_color_inactive'}`}>Лента заказов</Link>
+        </nav>
       </div>
       <div className={Styles.logo}>
         <Logo />
       </div>
       <div className='mt-4 mb-4'>
-        <AppHeaderItem id={'ProfileIcon'} name='Личный кабинет' link='/profile' activeMenuItem={activeMenuItem} setActiveMenuItem={setActiveMenuItem} />
+        <nav className='pl-5 pr-5 mr-2'>
+          <ProfileIcon type={pathname === '/profile' ? 'primary' : 'secondary'} />
+          <Link to='/profile' className={`ml-2 ${pathname !== '/profile' && 'text_color_inactive'}`}>Личный кабинет</Link>
+        </nav>
       </div>
     </header>
   )
