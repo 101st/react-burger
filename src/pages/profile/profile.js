06dbf9447ef3dad6d7aa2757eff74478cbe79c3d
3dspaun <3dspaun@gmail.com>
2022-10-24 12:35:01 +0300

    рефакторинг

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
-import { getUser } from "../../services/actions/auth";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { getUser } from '../../services/actions/auth';
 
-import styles from "./profile.module.scss";
-import { store } from "../../services/store";
+import styles from './profile.module.scss';
 
 function Profile() {
   const dispatch = useDispatch();
@@ -29,27 +28,27 @@ function Profile() {
       <div className={styles.menu + ' mr-20'}>
         <div>
           <Link to='/profile' className={styles.link}>
-            <p className="text text_type_main-medium">
+            <p className='text text_type_main-medium'>
               Профиль
             </p>
           </Link>
         </div>
         <div>
           <Link to={'/profile/orders'} className={styles.link}>
-            <p className="text text_type_main-medium">
+            <p className='text text_type_main-medium'>
               История заказов
             </p>
           </Link>
         </div>
         <div onClick={console.log}>
           <Link to={'/profile'} className={styles.link}>
-            <p className="text text_type_main-medium">
+            <p className='text text_type_main-medium'>
               Выход
             </p>
           </Link>
         </div>
         <div className={styles['add-info'] + ' mt-20'}>
-          <p className="text text_type_main-small">
+          <p className='text text_type_main-small'>
             В этом разделе вы можете изменить свои персональные данные
           </p>
         </div>
