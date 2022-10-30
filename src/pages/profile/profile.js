f7eddd7567c33fc6ad3f4c7f5250fba8519a2651
AYPotorochin <aleksandr_potorochin@rgs.ru>
2022-10-30 14:38:23 +0300

    исправлена работа подсветки ссылок в меню профеля пользователя

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { getUser, getLogout } from '../../services/actions/auth';
 
 import styles from './profile.module.scss';
 
 function Profile({ children }) {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
   const history = useHistory();
+
+  const isActive = (path) => {
+    if (pathname === path)
+      return styles['text_color_active'];
+    return 'text_color_inactive';
+  }
+
 
   useEffect(() => {
     dispatch(getUser());
@@ -17,14 +25,14 @@ function Profile({ children }) {
     <div className={styles.container + ' mt-20'}>
       <div className={styles.menu + ' mr-20'}>
         <div>
-          <Link to='/profile' className={styles.link}>
+          <Link to='/profile' className={`${styles.link} ${isActive('/profile')}`}>
             <p className='text text_type_main-medium'>
               Профиль
             </p>
           </Link>
         </div>
         <div>
-          <Link to={'/profile/orders'} className={styles.link}>
+          <Link to={'/profile/orders'} className={`${styles.link}  ${isActive('/profile/orders')}`}>
             <p className='text text_type_main-medium'>
               История заказов
             </p>
@@ -34,7 +42,7 @@ function Profile({ children }) {
           dispatch(getLogout());
           history.push('/login');
         }}>
-          <Link to={'/profile'} className={styles.link}>
+          <Link to={'/profile'} className={`${styles.link}  text_color_inactive`}>
             <p className='text text_type_main-medium'>
               Выход
             </p>
@@ -47,7 +55,7 @@ function Profile({ children }) {
         </div>
       </div>
       {children}
-    </div>
+    </div >
   )
 }
 
