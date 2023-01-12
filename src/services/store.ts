eedcb5bb5bcc2edbed5eb76b4610e4fd59465e5d
3dspaun <3dspaun@gmail.com>
2023-01-12 21:18:54 +0300

    рефакторинг

--- src/services/store.ts
+++ src/services/store.ts
@@ -6,6 +6,8 @@ import {
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
+//TODO нужно переписать
+//@ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
 export const store = legacy_createStore(rootReducer, enhancer);
