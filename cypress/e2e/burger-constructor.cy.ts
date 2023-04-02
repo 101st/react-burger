09e884b8a4d583368de8727687696ef0bab5c7a2
AYPotorochin <Aleksandr_Potorochin@rgs.ru>
2023-04-02 19:43:31 +0300

    fix:test: доработки, реф

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -1,27 +1,29 @@
 describe('service is available', function () {
   beforeEach(function () {
     cy.viewport(1450, 1100)
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
   })
 
+  const itemIngredient = '[data-cy="item-ingredient"]';
+  const constructorForm = '[data-cy="constructor-form"]';
 
-  it('should be available on localhost:3000', function () {
-    cy.get('[data-cy="item-ingredient"]').contains('Краторная булка N-200i').click();
+  it(`should be available on ${URL}`, function () {
+    cy.get(itemIngredient).contains('Краторная булка N-200i').click();
     cy.get('[data-cy="modal"]').contains("Детали ингредиента");
     cy.wait(1000);
     cy.get('[data-cy="modal-close"]').click();
 
-    cy.get('[data-cy="item-ingredient"]').contains("Флюоресцентная булка R2-D3").trigger('dragstart');
-    cy.get('[data-cy="constructor-form"]').trigger('drop');
+    cy.get(itemIngredient).contains("Флюоресцентная булка R2-D3").trigger('dragstart');
+    cy.get(constructorForm).trigger('drop');
     cy.wait(1000);
-    cy.get('[data-cy="item-ingredient"]').contains("Плоды Фалленианского дерева").trigger('dragstart');
-    cy.get('[data-cy="constructor-form"]').trigger('drop');
+    cy.get(itemIngredient).contains("Плоды Фалленианского дерева").trigger('dragstart');
+    cy.get(constructorForm).trigger('drop');
     cy.wait(1000);
-    cy.get('[data-cy="item-ingredient"]').contains("Соус традиционный галактический").trigger('dragstart');
-    cy.get('[data-cy="constructor-form"]').trigger('drop');
+    cy.get(itemIngredient).contains("Соус традиционный галактический").trigger('dragstart');
+    cy.get(constructorForm).trigger('drop');
     cy.wait(1000);
-    cy.get('[data-cy="item-ingredient"]').contains("Филе Люминесцентного тетраодонтимформа").trigger('dragstart');
-    cy.get('[data-cy="constructor-form"]').trigger('drop');
+    cy.get(itemIngredient).contains("Филе Люминесцентного тетраодонтимформа").trigger('dragstart');
+    cy.get(constructorForm).trigger('drop');
     cy.wait(1000);
     cy.get("button").contains("Оформить заказ").click();
 
